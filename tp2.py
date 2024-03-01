c5fca713eabb5fa86152c798bb2eba21806c4f64
Lorenzo <martinelli.lorenzo12@gmail.com>
2024-03-01 16:38:48 -0300

    Ejercicio 1

--- tp2.py
+++ tp2.py
@@ -22,6 +22,8 @@ letras : dict = {'0' : 'A', 1 : 'B', 2 : 'C', 3 : 'D', 4 : 'E', 5 : 'F', 6 : 'G'
                  14 : 'O', 15 : 'P', 16 : 'Q', 17: 'R', 18 : 'S', 19: 'T', 20: 'U',
                  21: 'V', 22 : 'W', 23 : 'X', 24: 'Y', 25 : 'Z'}
 
+#%% Análisis Exploratorio de Datos.
+
 # Separo la columna con los label.
 Y = df_sign['label']
 X = df_sign.drop(['label'], axis=1)
@@ -173,6 +175,10 @@ del Xc
 
 # Podemos ver ahora, cuales son exactamente los atributos que identifican a la leta C. Y, además
 # determinar con esto donde esta la mayor semejanza entre estas.
+
+#%% Construccion de modelo.
+
+
 
 
 
